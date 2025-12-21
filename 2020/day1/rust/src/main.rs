ab8e346201b1767191333fc6164fe8cc3f1a444c
ArchMinion <lawliet@hotmail.se>
2025-12-21 21:27:17 +0100

    Added time benchmark to rust and updated helper files.

--- 2020/day1/rust/src/main.rs
+++ 2020/day1/rust/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::time::Instant;
 
 fn main() {
     let test_path : &str = "src/test.txt";
@@ -25,6 +26,7 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 fn part_one(file_path : &str) {
+    let now = Instant::now();
     // Parse the problem
     let mut values : Vec<i64> = Vec::new();
     if let Ok(lines) = read_lines(file_path) {
@@ -51,10 +53,12 @@ fn part_one(file_path : &str) {
         }
     }
 
-    println!("Answer: {}", answer);
+    let elapsed = now.elapsed();
+    println!("Answer: {} Time: {} ms", answer, elapsed.as_millis());
 }
 
 fn part_two(file_path : &str) {
+    let now = Instant::now();
     // Parse the problem
     let mut values : Vec<i64> = Vec::new();
     if let Ok(lines) = read_lines(file_path) {
@@ -88,5 +92,6 @@ fn part_two(file_path : &str) {
         }
     }
 
-    println!("Answer: {}", answer);
+    let elapsed = now.elapsed();
+    println!("Answer: {} Time: {} ms", answer, elapsed.as_millis());
 }
