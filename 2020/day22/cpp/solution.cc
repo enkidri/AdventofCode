7567ebe13afa486dd6fe55ba8a97750e5dc812f9
drigon <lawliet@hotmail.se>
2026-06-18 18:22:38 +0200

    Added day 22 cpp version

--- 2020/day22/cpp/solution.cc
+++ 2020/day22/cpp/solution.cc
@@ -5,34 +5,66 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <unordered_set>
 
 using namespace std;
 
-class GameHandler {
+class GameState {
 public:
-  void init(const string &file_path) {
-    ifstream file(file_path);
-    string input;
+  virtual ~GameState() = default;
+  virtual void step() = 0;
 
-    char current_player_val;
-    while (getline(file, input)) {
-      if (input.size() >= 9) {
-        current_player_val = input[7];
-        continue;
-      } else if (input.empty()) {
-        continue;
-      }
-
-      if (current_player_val == '1') {
-        deck1.push_back(stoi(input));
-      } else if (current_player_val == '2') {
-        deck2.push_back(stoi(input));
-      }
-    }
+  bool is_ended() noexcept {
+    return deck1.empty() || deck2.empty() || loop_detected;
   }
 
-  // perform one round
-  void step() {
+  bool is_init() noexcept { return !deck1.empty() && !deck2.empty(); }
+
+  bool deck1_is_winner() {
+    // return true if deck1 is the winner, false otherwise
+    if (!is_ended()) {
+      throw std::runtime_error(
+          "Winner check failed. Cannot check winner when game under progress");
+    }
+    return deck2.empty() || loop_detected;
+  }
+
+  string serialize_state() {
+    string s;
+    for (int x : deck1)
+      s += to_string(x) + ",";
+    s += "|";
+    for (int x : deck2)
+      s += to_string(x) + ",";
+    return s;
+  }
+
+  void update_seen_states(const string &s) { seen_states.insert(s); }
+
+  void print() {
+    cout << "========" << endl;
+    cout << "Player 1:" << endl;
+    for (const auto val : deck1) {
+      cout << val << endl;
+    }
+    cout << endl;
+    cout << "Player 2:" << endl;
+    for (const auto val : deck2) {
+      cout << val << endl;
+    }
+    cout << "========" << endl;
+  }
+
+  deque<int> deck1;
+  deque<int> deck2;
+  string serialized_decks;
+  unordered_set<string> seen_states;
+  bool loop_detected = false;
+};
+
+class NormalGameState : public GameState {
+public:
+  void step() override {
     int p1_top = deck1.front();
     deck1.pop_front();
     int p2_top = deck2.front();
@@ -48,27 +80,105 @@ public:
       throw std::runtime_error("Scores are equal. Case not covered.");
     }
   }
+};
 
-  bool is_ended() noexcept { return deck1.empty() || deck2.empty(); }
+class RecursiveGameState : public GameState {
+public:
+  void step() override {
+    int p1_top = deck1.front();
+    deck1.pop_front();
+    int p2_top = deck2.front();
+    deck2.pop_front();
+
+    // prioritize the new recursive rules
+    if (p1_top <= deck1.size() && p2_top <= deck2.size()) {
+      // then we recurse!
+      RecursiveGameState rstate;
+      rstate.deck1.assign(deck1.begin(), deck1.begin() + p1_top);
+      rstate.deck2.assign(deck2.begin(), deck2.begin() + p2_top);
+      while (!rstate.is_ended()) {
+        rstate.step();
+      }
+      // winner of the recursive game is decided
+      if (rstate.deck1_is_winner()) {
+        deck1.push_back(p1_top);
+        deck1.push_back(p2_top);
+      } else {
+        deck2.push_back(p2_top);
+        deck2.push_back(p1_top);
+      }
+      // the normal game rules here
+    } else if (p1_top > p2_top) {
+      deck1.push_back(p1_top);
+      deck1.push_back(p2_top);
+    } else if (p1_top < p2_top) {
+      deck2.push_back(p2_top);
+      deck2.push_back(p1_top);
+    } else {
+      throw std::runtime_error("Scores are equal. Case not covered.");
+    }
+
+    // update the history cache
+    serialized_decks = serialize_state();
+    if (seen_states.count(serialized_decks)) {
+      loop_detected = true;
+    }
+    update_seen_states(serialized_decks);
+  }
+};
+
+class GameHandler {
+public:
+  void init(const string &file_path) {
+    if (state == nullptr) {
+      throw std::runtime_error(
+          "Start init failed. Initialize game state/type first.");
+    }
+
+    ifstream file(file_path);
+    string input;
+
+    char current_player_val;
+    while (getline(file, input)) {
+      if (input.size() >= 9) {
+        current_player_val = input[7];
+        continue;
+      } else if (input.empty()) {
+        continue;
+      }
+
+      if (current_player_val == '1') {
+        state->deck1.push_back(stoi(input));
+      } else if (current_player_val == '2') {
+        state->deck2.push_back(stoi(input));
+      }
+    }
+  }
+
+  void init_cache() { state->update_seen_states(state->serialize_state()); }
+
+  void set_game_state(std::unique_ptr<GameState> new_state) {
+    state = std::move(new_state);
+  }
 
   void start() {
-    if (deck1.empty() || deck2.empty()) {
+    if (!state->is_init()) {
       throw std::runtime_error(
           "Start executation failed. Initialize deck before running.");
     }
 
-    while (!is_ended()) {
-      step();
+    while (!state->is_ended()) {
+      state->step();
     }
   }
 
   uint64_t compute_score() {
-    if (deck1.empty() && deck2.empty()) {
+    if (!state->is_ended()) {
       throw std::runtime_error(
           "Computation of score failed. Run the game before computing score.");
     }
 
-    const deque<int> &deck = deck1.empty() ? deck2 : deck1;
+    const deque<int> &deck = state->deck1.empty() ? state->deck2 : state->deck1;
     uint64_t score = 0;
     for (int i = deck.size() - 1, j = 1; i >= 0; i--, j++) {
       score += deck[i] * j;
@@ -77,21 +187,8 @@ public:
     return score;
   }
 
-  void print() {
-    cout << "Player 1:" << endl;
-    for (const auto val : deck1) {
-      cout << val << endl;
-    }
-    cout << endl;
-    cout << "Player 2:" << endl;
-    for (const auto val : deck2) {
-      cout << val << endl;
-    }
-  }
-
 private:
-  deque<int> deck1;
-  deque<int> deck2;
+  std::unique_ptr<GameState> state = nullptr;
 };
 
 void partTwo(const string &file_path) {
@@ -100,8 +197,12 @@ void partTwo(const string &file_path) {
   string input;
   uint64_t answer = 0;
 
-  while (getline(file, input)) {
-  }
+  GameHandler handler;
+  handler.set_game_state(std::make_unique<RecursiveGameState>());
+  handler.init(file_path);
+  handler.init_cache();
+  handler.start();
+  answer = handler.compute_score();
 
   auto end = chrono::high_resolution_clock::now();
   std::chrono::duration<double, std::milli> elapsed = end - start;
@@ -114,6 +215,7 @@ void partOne(const string &file_path) {
   uint64_t answer = 0;
 
   GameHandler handler;
+  handler.set_game_state(std::make_unique<NormalGameState>());
   handler.init(file_path);
   handler.start();
   answer = handler.compute_score();
@@ -134,13 +236,13 @@ int main() {
   cout << "Input: ";
   partOne(my_path);
 
-  // cout << endl;
+  cout << endl;
 
-  // cout << "=========PART 2=========" << endl;
-  // cout << "Test: ";
-  // partTwo(test_path);
-  // cout << "Input: ";
-  // partTwo(my_path);
+  cout << "=========PART 2=========" << endl;
+  cout << "Test: ";
+  partTwo(test_path);
+  cout << "Input: ";
+  partTwo(my_path);
 
   return 0;
 };
